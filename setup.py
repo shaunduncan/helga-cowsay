3d3d6a65a67896c2bf4050560542de41eadc30e9
Michael Orr <michael@orr.co>
2014-09-15 14:43:18 -0400

    tweaks for distribution

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup, find_packages
 
-version = '1.0.0'
+version = '1.1.0'
 
 setup(name="helga-cowsay",
       version=version,
       description=('cowsay'),
-      classifiers=['Development Status :: 1 - Beta',
-                   'Environment :: IRC',
-                   'Intended Audience :: Twisted Developers, IRC Bot Developers',
-                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Software Development :: Libraries :: Python Modules',
-                   'Topic :: IRC Bots'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+           'Operating System :: OS Independent',
+           'Programming Language :: Python',
+           'Topic :: Software Development :: Libraries :: Python Modules',
+           'Topic :: Communications :: Chat :: Internet Relay Chat'],
       keywords='irc bot cow cowsay',
-      author='michael orr',
+      author='Michael Orr',
       author_email='michael@orr.co',
       url='https://github.com/michaelorr/helga-cowsay',
       license='LICENSE',
@@ -23,7 +22,7 @@ setup(name="helga-cowsay",
       py_modules=['helga-cowsay'],
       zip_safe=True,
       entry_points = dict(
-          helga_plugins = [
+          helga_plugins=[
               'cowsay= helga_cowsay:cowsay',
           ],
       ),
